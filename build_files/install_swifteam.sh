aeddbb8322b3982fe8d7aca9322cf01c5dc5904b
yogechou <yoge@swif.ai>
2025-11-28 22:04:58 +0900

    use beta version for testing

--- build_files/install_swifteam.sh
+++ build_files/install_swifteam.sh
@@ -1,7 +1,7 @@
 sudo tee /etc/yum.repos.d/swifteam.repo > /dev/null <<'EOF'
 [swifteam]
 name=Swifteam Repository
-baseurl=https://swif-linux-package.s3.ap-southeast-1.amazonaws.com/fedora/main/x86_64
+baseurl=https://swif-linux-package.s3.ap-southeast-1.amazonaws.com/fedora/beta/x86_64
 enabled=1
 gpgcheck=0
 EOF
