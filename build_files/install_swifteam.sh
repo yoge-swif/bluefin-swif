a07dd9394cd87221e3c35a6d1b2600b4175dc8f2
yogechou <yoge@swif.ai>
2025-12-02 11:56:26 +0900

    update repo

--- build_files/install_swifteam.sh
+++ build_files/install_swifteam.sh
@@ -1,20 +1,14 @@
-echo "=== OS Install Post ==="
-rpm --eval '%__os_install_post'
-
 sudo tee /etc/yum.repos.d/swifteam.repo > /dev/null <<'EOF'
 [swifteam]
 name=Swifteam Repository
-baseurl=https://swif-linux-package.s3.ap-southeast-1.amazonaws.com/fedora/beta/x86_64
+baseurl=https://swif-linux-package.s3.amazonaws.com/fedora/beta/x86_64
 enabled=1
-gpgcheck=0
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://swif-linux-package.s3.amazonaws.com/RPM-GPG-KEY-swifteam.pub
 EOF
 
-sudo dnf install -y \
-    rpm-build \
-    redhat-rpm-config \
-    fedora-release \
-    fedora-repos \
-    fedora-repos-modular
+
 
 sudo dnf makecache
 sudo dnf install swifteam -y
