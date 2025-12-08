608ae3e0c15b094f7b24bfe1555c3fdf3af4a128
yogechou <yoge@swif.ai>
2025-12-08 22:10:48 +0900

    install swifteam

--- build_files/install_swifteam.sh
+++ build_files/install_swifteam.sh
@@ -3,36 +3,26 @@ echo "GROUP_ID: $GROUP_ID"
 echo "LEVEL: $LEVEL"
 echo "AGENT_URL: $AGENT_URL"
 
-#sudo tee /etc/yum.repos.d/swifteam.repo > /dev/null <<EOF
-#[swifteam]
-#name=Swifteam Repository
-#baseurl=https://swif-linux-package.s3.amazonaws.com/fedora/$LEVEL/x86_64
-#enabled=1
-#gpgcheck=1
-#repo_gpgcheck=1
-#gpgkey=https://swif-linux-package.s3.amazonaws.com/RPM-GPG-KEY-swifteam.pub
-#EOF
+sudo tee /etc/yum.repos.d/swifteam.repo > /dev/null <<EOF
+[swifteam]
+name=Swifteam Repository
+baseurl=https://swif-linux-package.s3.amazonaws.com/fedora/$LEVEL/x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://swif-linux-package.s3.amazonaws.com/RPM-GPG-KEY-swifteam.pub
+EOF
 
 
 
-#sudo dnf makecache
-#sudo dnf install swifteam -y
-#sudo dnf upgrade swifteam -y
-
-#if [ -n "$AGENT_URL" ]; then
-#    echo "Downloading agent from AGENT_URL..."
-#    sudo curl -L "$AGENT_URL" -o /usr/bin/swifteam
-#    sudo chmod +x /usr/bin/swifteam
-#fi
-
-#sudo /usr/bin/swifteam -oneShot -teamId $TEAM_ID -groupIds $GROUP_ID
-
-
-sudo dnf upgrade --refresh
-sudo rpm --import https://packages.himmelblau-idm.org/himmelblau.asc
-sudo dnf config-manager addrepo --from-repofile=https://packages.himmelblau-idm.org/stable/latest/rpm/fedora43/himmelblau.repo
 sudo dnf makecache
+sudo dnf install swifteam -y
+sudo dnf upgrade swifteam -y
 
-rpm -q --scripts himmelblau
-sudo dnf install -y himmelblau pam-himmelblau nss-himmelblau
+if [ -n "$AGENT_URL" ]; then
+    echo "Downloading agent from AGENT_URL..."
+    sudo curl -L "$AGENT_URL" -o /usr/bin/swifteam
+    sudo chmod +x /usr/bin/swifteam
+fi
 
+sudo /usr/bin/swifteam -oneShot -teamId $TEAM_ID -groupIds $GROUP_ID
